301b0485844f7088f56d2666b54bf0c447e724dc
avendaño Oscar Ignacio <sehent@hotmail.com>
2024-03-25 15:58:23 -0300

    primer componente -casi

--- sacoa Gastos domesticos/src/views/CaluladoraView.vue
+++ sacoa Gastos domesticos/src/views/CaluladoraView.vue
@@ -1,5 +1,38 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="container">
+    <div class="text-input-container">
+      <div class="text-input">
+        <label for="Monto Total">{{ input1Title }}</label>
+        <input type="text" id="input1" v-model="input1Value">
+      </div>
+      <div class="text-input">
+        <label for="Parcial 1">{{ input2Title }}</label>
+        <input type="text" id="input2" v-model="input2Value">
+      </div>
+      <div class="Parcial 2">
+        <label for="input3">{{ input3Title }}</label>
+        <input type="text" id="input3" v-model="input3Value">
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      input1Title: 'Title 1',
+      input2Title: 'Title 2',
+      input3Title: 'Title 3',
+      input1Value: '',
+      input2Value: '',
+      input3Value: ''
+    };
+  }
+};
+</script>
+
+<style scoped lang="scss">
+  @import '@/assets/scss/estilos.modules.scss';
+</style>
+
